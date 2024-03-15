534c6275dfe0ad6000246283f165eeeb0c40f118
jmar dolorito <doloritojmar@gmail.com>
2024-03-16 05:20:38 +0800

    supplier design menu

--- InventorySystem1.0/frmsupplier.Designer.cs
+++ InventorySystem1.0/frmsupplier.Designer.cs
@@ -31,7 +31,6 @@
             this.components = new System.ComponentModel.Container();
             this.Panel1 = new System.Windows.Forms.Panel();
             this.txt_FirstName = new System.Windows.Forms.TextBox();
-            this.lbl_id = new System.Windows.Forms.Label();
             this.cbo_type_supplier = new System.Windows.Forms.ComboBox();
             this.Label1 = new System.Windows.Forms.Label();
             this.txt_Mobile = new System.Windows.Forms.TextBox();
@@ -47,6 +46,8 @@
             this.btn_delete = new System.Windows.Forms.Button();
             this.ToolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.btn_New = new System.Windows.Forms.Button();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.Panel1.SuspendLayout();
             this.GroupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtg_listSupplier)).BeginInit();
@@ -54,8 +55,9 @@
             // 
             // Panel1
             // 
-            this.Panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Panel1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Panel1.Controls.Add(this.label5);
             this.Panel1.Controls.Add(this.txt_FirstName);
             this.Panel1.Controls.Add(this.lbl_id);
             this.Panel1.Controls.Add(this.cbo_type_supplier);
@@ -75,24 +77,11 @@
             // txt_FirstName
             // 
             this.txt_FirstName.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_FirstName.Location = new System.Drawing.Point(324, 28);
+            this.txt_FirstName.Location = new System.Drawing.Point(540, 32);
             this.txt_FirstName.Margin = new System.Windows.Forms.Padding(4);
             this.txt_FirstName.Name = "txt_FirstName";
-            this.txt_FirstName.Size = new System.Drawing.Size(372, 26);
+            this.txt_FirstName.Size = new System.Drawing.Size(285, 26);
             this.txt_FirstName.TabIndex = 1;
-            // 
-            // lbl_id
-            // 
-            this.lbl_id.AutoSize = true;
-            this.lbl_id.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_id.Location = new System.Drawing.Point(343, 32);
-            this.lbl_id.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lbl_id.Name = "lbl_ID";
-            this.lbl_id.Size = new System.Drawing.Size(23, 22);
-            this.lbl_id.TabIndex = 9;
-            this.lbl_id.Text = "ID";
-            this.lbl_id.Visible = false;
-            this.lbl_id.TextChanged += new System.EventHandler(this.lbl_id_TextChanged);
             // 
             // cbo_type_supplier
             // 
@@ -101,10 +90,10 @@
             this.cbo_type_supplier.Items.AddRange(new object[] {
             "Administrator",
             "Staff"});
-            this.cbo_type_supplier.Location = new System.Drawing.Point(324, 143);
+            this.cbo_type_supplier.Location = new System.Drawing.Point(540, 162);
             this.cbo_type_supplier.Margin = new System.Windows.Forms.Padding(4);
             this.cbo_type_supplier.Name = "cbo_type_supplier";
-            this.cbo_type_supplier.Size = new System.Drawing.Size(372, 28);
+            this.cbo_type_supplier.Size = new System.Drawing.Size(285, 28);
             this.cbo_type_supplier.TabIndex = 4;
             this.cbo_type_supplier.Text = "SUPPLIER";
             this.cbo_type_supplier.SelectedIndexChanged += new System.EventHandler(this.cbo_type_supplier_SelectedIndexChanged);
@@ -112,10 +101,10 @@
             // Label1
             // 
             this.Label1.AutoSize = true;
-            this.Label1.BackColor = System.Drawing.Color.White;
+            this.Label1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.Label1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Label1.Location = new System.Drawing.Point(207, 32);
+            this.Label1.Location = new System.Drawing.Point(388, 32);
             this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label1.Name = "Label1";
             this.Label1.Size = new System.Drawing.Size(97, 22);
@@ -125,20 +114,20 @@
             // txt_Mobile
             // 
             this.txt_Mobile.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Mobile.Location = new System.Drawing.Point(324, 106);
+            this.txt_Mobile.Location = new System.Drawing.Point(540, 116);
             this.txt_Mobile.Margin = new System.Windows.Forms.Padding(4);
             this.txt_Mobile.Name = "txt_Mobile";
-            this.txt_Mobile.Size = new System.Drawing.Size(372, 26);
+            this.txt_Mobile.Size = new System.Drawing.Size(285, 26);
             this.txt_Mobile.TabIndex = 3;
             this.txt_Mobile.TextChanged += new System.EventHandler(this.txt_Mobile_TextChanged);
             // 
             // Label2
             // 
             this.Label2.AutoSize = true;
-            this.Label2.BackColor = System.Drawing.Color.White;
+            this.Label2.BackColor = System.Drawing.Color.PaleTurquoise;
             this.Label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Label2.Location = new System.Drawing.Point(213, 73);
+            this.Label2.Location = new System.Drawing.Point(390, 73);
             this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label2.Name = "Label2";
             this.Label2.Size = new System.Drawing.Size(95, 22);
@@ -148,32 +137,32 @@
             // Label4
             // 
             this.Label4.AutoSize = true;
-            this.Label4.BackColor = System.Drawing.Color.White;
+            this.Label4.BackColor = System.Drawing.Color.PaleTurquoise;
             this.Label4.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Label4.Location = new System.Drawing.Point(248, 146);
+            this.Label4.Location = new System.Drawing.Point(410, 162);
             this.Label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label4.Name = "Label4";
-            this.Label4.Size = new System.Drawing.Size(53, 22);
-            this.Label4.TabIndex = 6;
-            this.Label4.Text = "Type :";
+            this.Label4.Size = new System.Drawing.Size(57, 22);
+            this.Label4.TabIndex = 5;
+            this.Label4.Text = "Type  :";
             // 
             // txt_LastName
             // 
             this.txt_LastName.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_LastName.Location = new System.Drawing.Point(324, 69);
+            this.txt_LastName.Location = new System.Drawing.Point(540, 73);
             this.txt_LastName.Margin = new System.Windows.Forms.Padding(4);
             this.txt_LastName.Name = "txt_LastName";
-            this.txt_LastName.Size = new System.Drawing.Size(372, 26);
+            this.txt_LastName.Size = new System.Drawing.Size(285, 26);
             this.txt_LastName.TabIndex = 2;
             // 
             // Label3
             // 
             this.Label3.AutoSize = true;
-            this.Label3.BackColor = System.Drawing.Color.White;
+            this.Label3.BackColor = System.Drawing.Color.PaleTurquoise;
             this.Label3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Label3.Location = new System.Drawing.Point(231, 110);
+            this.Label3.Location = new System.Drawing.Point(410, 116);
             this.Label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label3.Name = "Label3";
             this.Label3.Size = new System.Drawing.Size(70, 22);
@@ -201,6 +190,7 @@
             this.dtg_listSupplier.AllowUserToResizeColumns = false;
             this.dtg_listSupplier.AllowUserToResizeRows = false;
             this.dtg_listSupplier.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtg_listSupplier.BackgroundColor = System.Drawing.Color.White;
             this.dtg_listSupplier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtg_listSupplier.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dtg_listSupplier.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
@@ -267,7 +257,7 @@
             // 
             this.btn_delete.BackColor = System.Drawing.Color.Transparent;
             this.btn_delete.Enabled = false;
-            this.btn_delete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_delete.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btn_delete.Location = new System.Drawing.Point(415, 217);
             this.btn_delete.Margin = new System.Windows.Forms.Padding(4);
@@ -294,10 +284,37 @@
             this.btn_New.UseVisualStyleBackColor = false;
             this.btn_New.Click += new System.EventHandler(this.btn_New_Click);
             // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_id.Location = new System.Drawing.Point(536, 32);
+            this.lbl_id.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(24, 22);
+            this.lbl_id.TabIndex = 9;
+            this.lbl_id.Text = "ID";
+            this.lbl_id.Visible = false;
+            this.lbl_id.TextChanged += new System.EventHandler(this.lbl_id_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 49.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.Location = new System.Drawing.Point(24, 44);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(317, 98);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Supplier";
+            // 
             // frmsupplier
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
             this.ClientSize = new System.Drawing.Size(1004, 639);
             this.Controls.Add(this.Panel1);
             this.Controls.Add(this.GroupBox1);
@@ -323,7 +340,6 @@
 
         internal System.Windows.Forms.Panel Panel1;
         internal System.Windows.Forms.TextBox txt_FirstName;
-        internal System.Windows.Forms.Label lbl_id;
         internal System.Windows.Forms.ComboBox cbo_type_supplier;
         internal System.Windows.Forms.Label Label1;
         internal System.Windows.Forms.TextBox txt_Mobile;
@@ -339,5 +355,7 @@
         internal System.Windows.Forms.Button btn_update;
         internal System.Windows.Forms.Button btn_delete;
         internal System.Windows.Forms.Button btn_New;
+        internal System.Windows.Forms.Label lbl_id;
+        internal System.Windows.Forms.Label label5;
     }
 }
